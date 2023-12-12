8baf50197084cbf7b2708f4b4760dacfecc8b9de
Athul Raj <mail@athulraj.com>
2023-12-12 19:12:24 +0530

    Init

---
+++ src/ScaleXYLaravelToolsServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ScaleXY\Tools;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScaleXYLaravelToolsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
