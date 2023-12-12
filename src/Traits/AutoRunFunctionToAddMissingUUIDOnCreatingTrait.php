f4920f11d6a9e462ad90ceb87f9a3428401bf8e1
Athul Raj <mail@athulraj.com>
2023-12-12 19:18:27 +0530

    AutoRunFunctionToAddMissingUUIDOnCreatingTrait

---
+++ src/Traits/AutoRunFunctionToAddMissingUUIDOnCreatingTrait.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ScaleXY\Tools\Traits;
+
+trait AutoRunFunctionToAddMissingUUIDOnCreatingTrait
+{
+	public function AutoRunFunctionToAddMissingUUIDOnCreating($instance)
+	{
+		if(is_null($instance->uuid))
+			$instance->uuid = \Illuminate\Support\Str::uuid();
+	}
+}
