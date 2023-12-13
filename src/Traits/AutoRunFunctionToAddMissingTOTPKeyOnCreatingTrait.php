f5c36ffc05f3b7f94821d43568d58a5b4fb1cae6
Athul Raj <mail@athulraj.com>
2023-12-13 13:38:00 +0530

    TOTP

---
+++ src/Traits/AutoRunFunctionToAddMissingTOTPKeyOnCreatingTrait.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ScaleXY\Tools\Traits;
+
+trait AutoRunFunctionToAddMissingTOTPKeyOnCreatingTrait
+{
+	public function AutoRunFunctionToAddMissingTOTPKeyOnCreating($instance)
+	{
+		if(is_null($instance->totp_key))
+			$instance->totp_key = (\OTPHP\TOTP::create())->getSecret();
+	}
+}
