4f17e042c2c53e95ba4b31e534c657c88a9f70b4
Athul Raj <mail@athulraj.com>
2024-01-01 13:52:52 +0530

    HasFunction

---
+++ src/Traits/AutoRunFunctionToHashPasswordOnCreatingTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ScaleXY\Tools\Traits;
+
+trait AutoRunFunctionToHashPasswordOnCreatingTrait
+{
+	public function AutoRunFunctionToHashPasswordOnCreating($instance)
+	{
+		if (strlen($instance->Password) > 0) {
+			$instance->Password = \Illuminate\Support\Facades\Hash::make($instance->Password);
+		} else {
+			unset($instance->Password);
+		}
+	}
+}
---
+++ src/Traits/AutoRunFunctionToHashPasswordOnUpdatingTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ScaleXY\Tools\Traits;
+
+trait AutoRunFunctionToHashPasswordOnUpdatingTrait
+{
+	public function AutoRunFunctionToHashPasswordOnUpdating($instance)
+	{
+		if (strlen($instance->Password) > 0) {
+			if ($instance->Password != self::find($instance->id)->Password)
+				$instance->Password = \Illuminate\Support\Facades\Hash::make($instance->Password);
+		} else {
+			unset($instance->Password);
+		}
+	}
+}
