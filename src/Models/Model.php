f6642578f84410733e8592367a4cadd12d2c51ab
Athul Raj <mail@athulraj.com>
2024-02-06 12:18:53 +0530

    Remove BasicModel trait

---
+++ src/Models/Model.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ScaleXY\Tools\Models;
+
+class Model extends \Illuminate\Database\Eloquent\Model
+{
+	public $timestamps = true;
+	protected $guarded = ['id'];
+}
--- src/Traits/BasicModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ScaleXY\Tools\Traits;
-
-trait BasicModel
-{
-	public $timestamps = true;
-	protected $guarded = ['id'];
-}
