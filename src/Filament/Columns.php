77b7a585f910bced6b68ad827d930f36df8452f0
Athul Raj <mail@athulraj.com>
2024-06-25 14:29:58 +0530

    UUID Column for Filament Table

---
+++ src/Filament/Columns.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ScaleXY\Tools\Filament;
+
+class Columns
+{
+    public function UUIDColumn()
+    {
+        return \Filament\Tables\Columns\TextColumn::make('uuid')
+            ->label('UUID')
+            ->searchable()
+            ->formatStateUsing(fn (string $state) => substr($state, -12))
+            ->copyable()
+            ->copyMessage('Copied!')
+            ->copyableState(fn (string $state) => $state);
+    }
+}
