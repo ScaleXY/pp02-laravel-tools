c0897a6b90be735badcb3baa631165c0fc8e587c
Athul Raj <mail@athulraj.com>
2025-02-01 10:22:39 +0530

    fix

--- src/Helpers/failAPIRequest.php
+++ src/Helpers/failAPIRequest.php
@@ -3,13 +3,13 @@
 function failAPIRequest($message = 'An error occurred', $code = 422)
 {
     $class_app = 'ScaleXY\\Tools\\Exceptions\\E'.$code;
-    if (class_exists($class_app)) {
-        throw new $class_app($message, $code);
-    }
     if (config('app.api_debug')) {
         \Illuminate\Support\Facades\Log::error('failAPIRequest: ['
             .debug_backtrace(1)[0]['class'].': L'.debug_backtrace(1)[0]['line']
             .'] '.($message ?? 'NO_MESSAGE'));
+    }
+    if (class_exists($class_app)) {
+        throw new $class_app($message, $code);
     }
     throw new Exception($message, $code);
 }
