092b4f1333ed89151f0d7ff22fcd619f7107c383
Athul Raj <mail@athulraj.com>
2025-06-14 20:12:03 +0530

    Add getPrintableLabel

--- src/Traits/MorphableIdentifiers.php
+++ src/Traits/MorphableIdentifiers.php
@@ -9,6 +9,10 @@ trait MorphableIdentifiers
         $returnValue = \Illuminate\Support\Str::of(get_class($this))->afterLast('\\').' ['.$this->getKey().']';
         if (method_exists($this, 'getLabel')) {
             $returnValue .= ' '.$this->getLabel();
+        } else {
+            if (method_exists($this, 'getPrintableLabel')) {
+                $returnValue .= ' '.$this->getPrintableLabel();
+            }
         }
 
         return $returnValue;
