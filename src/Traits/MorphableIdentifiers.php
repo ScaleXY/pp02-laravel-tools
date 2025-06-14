8d7f0f7123da6a6d94e49c55499362c3882bb9df
Athul Raj <mail@athulraj.com>
2025-06-14 20:08:43 +0530

    fix

--- src/Traits/MorphableIdentifiers.php
+++ src/Traits/MorphableIdentifiers.php
@@ -6,7 +6,7 @@ trait MorphableIdentifiers
 {
     public function getMorphLabel()
     {
-        $returnValue = basename($this).' ['.$this->getKey().']';
+        $returnValue = \Illuminate\Support\Str::of(get_class($this))->afterLast('\\').' ['.$this->getKey().']';
         if (method_exists($this, 'getLabel')) {
             $returnValue .= ' '.$this->getLabel();
         }
