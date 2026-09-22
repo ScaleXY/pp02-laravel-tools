a921a53e479e77a7c987b5d4dad9c45980e11663
Athul Raj <mail@athulraj.com>
2026-09-22 14:29:03 +0530

    Cleanup

--- src/Traits/AutoRunTrait.php
+++ src/Traits/AutoRunTrait.php
@@ -6,47 +6,16 @@ trait AutoRunTrait
 {
     protected static function booted()
     {
-        static::creating(function ($instance) {
-            foreach (get_class_methods($instance) as $function_name) {
-                if ((substr($function_name, 0, 17) == 'AutoRunFunctionTo') && (substr($function_name, -10) == 'OnCreating')) {
-                    $instance->{$function_name}($instance);
+        foreach (['creating', 'created', 'updating', 'updated', 'deleting', 'deleted'] as $event) {
+            $suffix = 'On'.ucfirst($event);
+
+            static::{$event}(function ($instance) use ($suffix) {
+                foreach (get_class_methods($instance) as $method) {
+                    if (str_starts_with($method, 'AutoRunFunctionTo') && str_ends_with($method, $suffix)) {
+                        $instance->{$method}($instance);
+                    }
                 }
-            }
-        });
-        static::created(function ($instance) {
-            foreach (get_class_methods($instance) as $function_name) {
-                if ((substr($function_name, 0, 17) == 'AutoRunFunctionTo') && (substr($function_name, -9) == 'OnCreated')) {
-                    $instance->{$function_name}($instance);
-                }
-            }
-        });
-        static::updating(function ($instance) {
-            foreach (get_class_methods($instance) as $function_name) {
-                if ((substr($function_name, 0, 17) == 'AutoRunFunctionTo') && (substr($function_name, -10) == 'OnUpdating')) {
-                    $instance->{$function_name}($instance);
-                }
-            }
-        });
-        static::updated(function ($instance) {
-            foreach (get_class_methods($instance) as $function_name) {
-                if ((substr($function_name, 0, 17) == 'AutoRunFunctionTo') && (substr($function_name, -9) == 'OnUpdated')) {
-                    $instance->{$function_name}($instance);
-                }
-            }
-        });
-        static::deleting(function ($instance) {
-            foreach (get_class_methods($instance) as $function_name) {
-                if ((substr($function_name, 0, 17) == 'AutoRunFunctionTo') && (substr($function_name, -10) == 'OnDeleting')) {
-                    $instance->{$function_name}($instance);
-                }
-            }
-        });
-        static::deleted(function ($instance) {
-            foreach (get_class_methods($instance) as $function_name) {
-                if ((substr($function_name, 0, 17) == 'AutoRunFunctionTo') && (substr($function_name, -9) == 'OnDeleted')) {
-                    $instance->{$function_name}($instance);
-                }
-            }
-        });
+            });
+        }
     }
 }
